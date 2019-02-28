30b0975a874d3ede2e33173ecf8635d82383e026
sunmory <xchh18@163.com>
2019-02-28 15:43:42 +0800

    Interleaving String comment

--- C++/DP/InterleavingString.cpp
+++ C++/DP/InterleavingString.cpp
@@ -9,18 +9,19 @@ class Solution {
 public:
 	bool isInterleave(string s1, string s2, string s3) {
 		/**
-		* @brief 函数简要说明-测试函数
-		* @param index    参数1
-		* @param t        参数2 @see CTest
-		*
-		* @return 返回说明
-		*     -<em>false</em> fail
-		*     -<em>true</em> succeed
+		* @brief 设已知字符串s3前t个字符可能由字符串s1的前t1个字符与字符串s2的前t2个字符交错排列而成,
+		*	 且有 t1 + t2 = t， 则考虑当前问题——匹配s3中第t+1个字符串，其共有两种情况：
+		     - 由 s1 的第 t1 + 1 个字符匹配 s3 的第 t + 1 个字符
+			 - 由 s2 的第 t2 + 1 个字符匹配 s3 的第 t + 1 个字符
+			 构建二维数组 bool_array 描述这一情况：
+			 则 bool_array[t1][t2] 为求解 bool_array[t1 + 1][t2] 与 bool_array[t1][t2 + 1] 的子问题。
+			 考虑到在 bool_array 数组构建时，从左至右按列构建，每次只需用到上一列的结果，所以可将内存空间简化至一维数组。
+			 当 bool_array[m + 1][n + 1]为true时，表示存在由s1与s2转换至s3的交错排列
 		*/
 		int m = s1.length();
 		int n = s2.length();
 
-		// 创建 m + 1 维数组以讨论字符串s1或字符串s2分别为s3起始字符串的情形
+		// 创建 m + 1 维数组以包含空字符来讨论字符串s1或字符串s2分别为s3起始字符串的情形
 		vector<bool> bool_array(m + 1, false);
 		bool_array[0] = true;
 
@@ -58,5 +59,4 @@ int main(){
 	string s3 = "dbbca";
 	bool result = s.isInterleave(s1, s2, s3);
 	cout << "Interleaving: " << result << endl;
-	//cout << s1.length() << endl;
 }
